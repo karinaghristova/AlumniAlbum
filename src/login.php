42f8cf5c94ae7611f9d1868f153e0b35d4c87870
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2024-01-21 04:19:35 +0200

    Removed unneccessary comments

--- src/login.php
+++ src/login.php
@@ -7,20 +7,16 @@ require_once "SessionRequestHandler.php";
 $result = null;
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        // echo "get";
         $result = (new SessionRequestHandler())->checkLoginStatus();
         break;
     case 'POST':
-        // echo "post";
         $result = (new SessionRequestHandler())->login($_POST['username'], $_POST['password']);
         break;
     case 'DELETE':
-        // echo "delete";
         (new SessionRequestHandler())->logout();
         $result = true;
         break;
     default:
-        // echo "default";
         $result = false; 
         break;
 }
--- src/myProfile.php
+++ src/myProfile.php
@@ -4,60 +4,48 @@ require_once "bootstrap.php";
 require_once "database.php";
 require_once "SessionRequestHandler.php";
 
-// Start the session
 session_start();
 
-// Initialize the result
 $result = ["userData" => null];
 
-// Check if the user is logged in
+// Check if user is logged in
 if (isset($_SESSION["username"])) {
     $username = $_SESSION["username"];
 
-    // Create an instance of SessionRequestHandler
     $sessionHandler = new SessionRequestHandler();
-
-    // Fetch the user's profile data
     $userData = $sessionHandler->getProfileData($username);
 
-    // Set the result data
     $result["userData"] = $userData;
 } else {
-    // Handle the case where the user is not logged in
     $result["error"] = "User not logged in";
 }
 
-// Return the result as JSON
 ob_clean();
 header('Content-Type: application/json');
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
 
-//Editing basic information
+// Basic information edit
 if (isset($_POST["editBasicInfo"])) {
     $newFirstName = $_POST["newFirstName"];
     $newLastName = $_POST["newLastName"];
     $newEmail = $_POST["newEmail"];
 
-    // Update user's information in the DB
     $sessionHandler->editBasicInformation($username, $newFirstName, $newLastName, $newEmail);
 
-    //Return response to the client
     $editResult = ["success" => true];
     echo json_encode($editResult);
     exit;
 }
 
-// Additional logic for editing academic information
+// Academic information edit
 if (isset($_POST["editAcademicInfo"])) {
     $newMajor = $_POST["newMajor"];
     $newClass = $_POST["newClass"];
     $newStream = $_POST["newStream"];
     $newAdministrativeGroup = $_POST["newAdministrativeGroup"];
 
-    // Update the academic information in the database
     $sessionHandler->editAcademicInformation($username, $newMajor, $newClass, $newStream, $newAdministrativeGroup);
 
-    // Optionally, you can return a response to the client
     $editResult = ["success" => true];
     echo json_encode($editResult);
     exit;
