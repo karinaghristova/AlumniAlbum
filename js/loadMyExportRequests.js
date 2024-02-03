c91b28ed60cd677781e483456416d51b3c59c5cc
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2024-02-04 00:33:03 +0200

    Added export to csv file for album export requests

--- js/loadMyExportRequests.js
+++ js/loadMyExportRequests.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     getAllPhotoExportRequests();
     getAllAlbumExportRequests();
 
-    // Add event listener for the button click
+    // Add event listener for photo export requests button
     document.getElementById("photoExportRequestsDownloadBtn").addEventListener("click", function () {
         fetch("../src/getAllPhotoExportRequests.php")
             .then(response => response.json())
@@ -13,6 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .catch(error => console.error("Error fetching photo export requests:", error.message));
     });
+
+
+    // Add event listener for album export requests button
+    document.getElementById("albumExportRequestsDownloadBtn").addEventListener("click", function () {
+      fetch("../src/getAllAlbumExportRequests.php")
+          .then(response => response.json())
+          .then(data => {
+              console.log("Data from response:");
+              console.log(data);
+              downloadAlbumExportRequestsAsCSV(data);
+          })
+          .catch(error => console.error("Error fetching photo export requests:", error.message));
+    });
+
+
+
 });
 
 function getAllPhotoExportRequests() {
@@ -148,6 +164,7 @@ function downloadPhotoExportRequestsAsCSV(data) {
         if (!request.photoId ||
             !request.photoName ||
             !request.serviceName ||
+            !request.count ||
             !request.requestSenderUsername ||
             !request.senderFirstName ||
             !request.senderLastName
@@ -160,6 +177,7 @@ function downloadPhotoExportRequestsAsCSV(data) {
             request.photoId, 
             request.photoName,
             request.serviceName,
+            request.count,
             request.requestSenderUsername,
             `${request.senderFirstName} ${request.senderLastName}`
         ].join(";");
@@ -167,11 +185,45 @@ function downloadPhotoExportRequestsAsCSV(data) {
     });
 
     // Combine header and data
-    const csvContent = ["ID на снимка;Име на снимка;Вид на услугата;Потребителско име на заявителя;Имена на заявителя"].concat(csvData).join("\n");
+    const csvContent = ["ID на снимка;Име на снимка;Вид на услугата;Брой;Потребителско име на заявителя;Имена на заявителя"].concat(csvData).join("\n");
 
     // Trigger the download
     downloadCSV(csvContent, 'photo_export_requests.csv');
 }
 
+function downloadAlbumExportRequestsAsCSV(data){
+    if (!data || !Array.isArray(data.albumExportRequests)) {
+        console.error("Data is not in correct format. Array is expected");
+        return;
+    }
+
+    const csvData = data.albumExportRequests.map(request => {
+        if (!request.albumId ||
+            !request.albumTitle ||
+            !request.count ||
+            !request.requestSenderUsername ||
+            !request.senderFirstName ||
+            !request.senderLastName
+        ) {
+            console.error("Missing export requests data");
+            return "";
+        }
+
+        const rowContent = [
+            request.albumId, 
+            request.albumTitle,
+            request.count,
+            request.requestSenderUsername,
+            `${request.senderFirstName} ${request.senderLastName}`
+        ].join(";");
+        return rowContent;
+    });
+
+    // Combine header and data
+    const csvContent = ["ID на албум;Име на албум;Брой;Потребителско име на заявителя;Имена на заявителя"].concat(csvData).join("\n");
+
+    // Trigger the download
+    downloadCSV(csvContent, 'album_export_requests.csv');
+}
 
 
